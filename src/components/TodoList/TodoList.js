08bdffaa52a92d4d4bb87de9a0fef59dc65b7e0d
AleksandrOzerov <36672682+AleksandrOzerov@users.noreply.github.com>
2018-11-05 21:23:28 +0300

    Update TodoList.js

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -8,18 +8,7 @@ const TodoList = ({ todos, onDeleted, onToggleImportant, onToggleDone }) => {
     const { id, ...itemProps } = item; //деструкторизацию и добавление rest параметра itemProps, в который войдут
     return (
       //все свойства объека которые не были деструторированы (все свойства кроме id)
-      /*
-<li>
-можно записать так:
-    <TodoListItem
-    label ={item.label}
-    important = {item.important}/>
-</li>
-
-этот код можно записать короче выполнив spread оператор для объекта, это означает
-взять каждое свойство из объекта item и передать в качестве атрибута вместе со значением
-в todoListItem
-*/
+  
       <li key={id} className="list-group-item">
         <TodoListItem {...itemProps} 
         onDeleted={()=>onDeleted(id)}
